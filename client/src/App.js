65eff1cf3ab24fecef9ef60d51b7b5d0480b7a5e
Jung <8735132@gmail.com>
2018-04-09 20:32:29 +0900

    client init

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="booklist">
+          <li>Book List</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
